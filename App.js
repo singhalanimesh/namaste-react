55942507e14f5aa62118d46ac9d79564ff3319f1
singhalanimesh <singhalanimesh06@gmail.com>
2024-01-12 05:58:57 +0530

    First

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", {}, "Im an h1 tag"),
+      React.createElement("h2", {}, "Im an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "Im an h1 tag"),
+      React.createElement("h2", {}, "Im an h2 tag"),
+    ])
+  ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
